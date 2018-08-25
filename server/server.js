42520bbc29068f0d002329fa7e77be63e9014518
Amrita Nath <amritanath@Amritas-MacBook-Air.local>
2018-08-25 12:30:25 -0400

    create generate message utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http =require('http');
 const express = require('express');
 const socketIO =require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const publicpath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -19,19 +21,9 @@ io.on('connection', (socket)=>{
 
 
   // socket.emit from Admin text welcome to chat app
-  socket.emit('newMessage', {
-    from : 'Admin',
-    text : 'Weclome to the chat app',
-    createdAt : new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Weclome to the chat app'));
   // socket.broadcast.emit from admin text New user joined
-
-  socket.broadcast.emit('newMessage', {
-    from : 'Admin',
-    text : 'Weclome to the chat app',
-    createdAt : new Date().getTime()
-
-  })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 
 
@@ -57,13 +49,18 @@ io.on('connection', (socket)=>{
 
 
 
+  // socket.on('createMessage', (message)=>{
+  //   console.log('createMessage', message);
+  //   io.emit('newMessage',{
+  //     from: message.from,
+  //     text: message.text,
+  //     createdAt :new Date().getTime()
+  //   });
+// !
+// v
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createdAt :new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from , message.text ));
 
 
     // socket.broadcast.emit('newMessage', {
